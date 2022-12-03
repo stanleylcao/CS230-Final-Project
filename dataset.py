02ed3f729755c9f723551044e2ef1a3692f48a09
Arthur Lee <Arthur@MacBook-Pro-104.local>
2022-12-03 17:15:05 +0800

    Add baseline and multi head models

--- dataset.py
+++ dataset.py
@@ -9,7 +9,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class ChessDataset(Dataset):
-    def __init__(self, csv_file_path) -> None:
+    def __init__(self, csv_file_path, sequence_model) -> None:
         df = pd.read_csv(csv_file_path)
         df.fillna(0, inplace=True)
         df = pd.get_dummies(data=df, columns=['ECO', 'Event', 'Termination', 'TimeControl',
@@ -25,6 +25,7 @@ class ChessDataset(Dataset):
         self.n_examples = df.shape[0] // self.n_steps
         self.n_features = df.shape[1]
         self.bin_width = 100
+        self.sequence_model = sequence_model
         #self.df, self.white_labels, self.black_labels = regression_to_softmax(df, ["WhiteElo", "BlackElo"], self.bin_width, 3000)
 
     def __len__(self):
@@ -38,4 +39,6 @@ class ChessDataset(Dataset):
         #df = df[["eval_normalized", "white_wins", "is_check"]]
         df = df.drop(columns=["WhiteElo", "BlackElo"])
         features_tensor = torch.tensor(df.values)  # (n_steps, n_features)
+        if not self.sequence_model:
+            features_tensor = torch.flatten(features_tensor)
         return features_tensor.double().to(device), labels_tensor[0, :].double().to(device)
